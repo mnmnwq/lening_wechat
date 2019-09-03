8c62a9ab8c1b9f84578a71ad4fc4e8d8ca730e86
www <mnmnwq@qq.com>
2019-09-03 19:48:34 +0800

    aa

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class LoginController extends Controller
 {
@@ -30,8 +31,33 @@ class LoginController extends Controller
         $req = $request->all();
         $result = file_get_contents('https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WECHAT_APPID').'&secret='.env('WECHAT_APPSECRET').'&code='.$req['code'].'&grant_type=authorization_code');
         $re = json_decode($result,1);
-        $user_info = file_get_contents('https://api.weixin.qq.com/sns/userinfo?access_token='.$re['access_token'].'&openid='.$re['openid'].'&lang=zh_CN');
-        $wechat_user_info = json_decode($user_info,1);
-        dd($wechat_user_info);
+       $user_info = file_get_contents('https://api.weixin.qq.com/sns/userinfo?access_token='.$re['access_token'].'&openid='.$re['openid'].'&lang=zh_CN');
+       $wechat_user_info = json_decode($user_info,1);
+        $openid = $re['openid'];
+        $wechat_info = DB::connection('mysql_cart')->table('user_wechat')->where(['openid'=>$openid])->first();
+        dd($wechat_info);
+        if(!empty($wechat_info)){
+            //存在,登陆
+            $request->session()->put('uid',$wechat_info->uid);
+            echo "ok";
+           // return redirect('');  //主页
+        }else{
+            //不存在,注册,登陆
+            //插入user表数据一条
+            DB::connection('mysql_cart')->beginTransaction();  //打开事物
+            $uid = DB::connection('mysql_cart')->table('user')->insertGetId([
+                'name'=>$wechat_user_info['nickname'],
+                'password'=>'',
+                'reg_time'=>time()
+            ]);
+            $insert_result = DB::connection('mysql_cart')->table('user_wechat')->insert([
+                'uid'=>$uid,
+                'openid'=>$openid
+            ]);
+            //登陆操作
+            $request->session()->put('uid',$wechat_info->uid);
+            echo "ok";
+            // return redirect('');  //主页
+        }
     }
 }
