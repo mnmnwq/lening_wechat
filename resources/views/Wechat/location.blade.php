4bbb6ad42751d601d7d6dc0608eeed58782eed77
www <mnmnwq@qq.com>
2019-09-19 10:34:11 +0800

    a

--- resources/views/Wechat/location.blade.php
+++ resources/views/Wechat/location.blade.php
@@ -19,13 +19,14 @@
 
     wx.ready(function(){
         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-        wx.openLocation({
-            latitude: 0, // 纬度，浮点数，范围为90 ~ -90
-            longitude: 0, // 经度，浮点数，范围为180 ~ -180。
-            name: '', // 位置名
-            address: '', // 地址详情说明
-            scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
-            infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+        wx.getLocation({
+            type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+            success: function (res) {
+                var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                var speed = res.speed; // 速度，以米/每秒计
+                var accuracy = res.accuracy; // 位置精度
+            }
         });
     });
 
